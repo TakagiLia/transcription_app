c91d9e84efef4d2912eadf1adf594452446af59c
TakagiRia <r.takagi.test@gmail.com>
2024-07-22 18:55:10 +0900

    AppBindsModuleの変更雨に伴う修正

--- app/src/main/java/biz/moapp/transcription_app/usecase/AudioUseCaseImpl.kt
+++ app/src/main/java/biz/moapp/transcription_app/usecase/AudioUseCaseImpl.kt
@@ -1,18 +1,12 @@
 package biz.moapp.transcription_app.usecase
 
-import android.content.Context
 import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.util.Log
-import javax.inject.Inject
 
-class AudioUseCaseImpl@Inject constructor(
-    context: Context
-)  :AudioUseCase {
+class AudioUseCaseImpl :AudioUseCase {
 
-    private val filePath : String = context.getExternalFilesDir(null)?.absolutePath + "/recording.m4a"
-
-    override fun recordingStart(recorder: MediaRecorder) {
+    override fun recordingStart(recorder: MediaRecorder, filePath:String) {
         recorder.apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
@@ -28,7 +22,7 @@ class AudioUseCaseImpl@Inject constructor(
         recorder.reset()
     }
 
-    override fun audioPlay(): MediaPlayer{
+    override fun audioPlay(filePath:String): MediaPlayer{
         return MediaPlayer().apply {
             try {
                 setDataSource(filePath)
