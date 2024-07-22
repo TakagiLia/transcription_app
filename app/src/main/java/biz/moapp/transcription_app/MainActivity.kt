0fb1be5c84cf9566a8409389c19b0708d9934480
TakagiRia <r.takagi.test@gmail.com>
2024-07-22 11:07:31 +0900

    アプリが起動している間は画面がロックされないようにする修正

--- app/src/main/java/biz/moapp/transcription_app/MainActivity.kt
+++ app/src/main/java/biz/moapp/transcription_app/MainActivity.kt
@@ -2,31 +2,34 @@ package biz.moapp.transcription_app
 
 import android.Manifest.permission.RECORD_AUDIO
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.speech.SpeechRecognizer
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import biz.moapp.transcription_app.ui.theme.MainScreen
-import biz.moapp.transcription_app.ui.theme.MainScreenViewModel
+import biz.moapp.transcription_app.ui.main.MainScreen
+import biz.moapp.transcription_app.ui.main.MainScreenViewModel
 import biz.moapp.transcription_app.ui.theme.Transcription_appTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private var speechRecognizer : SpeechRecognizer? = null
-
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        /**アプリが起動している間は画面がロックされないようにしている**/
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         setContent {
             val mainScreenViewModel: MainScreenViewModel by viewModels()
@@ -37,9 +40,8 @@ class MainActivity : ComponentActivity() {
                     if (granted != PackageManager.PERMISSION_GRANTED) {
                         ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO), PERMISSIONS_RECORD_AUDIO)
                     }
-                    speechRecognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
 
-                    MainScreen(modifier = Modifier.padding(innerPadding), speechRecognizer,mainScreenViewModel)
+                    MainScreen(modifier = Modifier.padding(innerPadding), mainScreenViewModel)
                 }
             }
         }
@@ -47,7 +49,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        speechRecognizer?.destroy()
     }
 
     companion object {
