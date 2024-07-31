280dcfa5baad204a90acb6b8df694201e5e6db2b
TakagiRia <r.takagi.test@gmail.com>
2024-07-31 17:04:57 +0900

    テーマカラーを初期状態から修正

--- app/src/main/java/biz/moapp/transcription_app/ui/theme/Color.kt
+++ app/src/main/java/biz/moapp/transcription_app/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Viridian = Color(0xff00885a)
+val PaleGreen = Color(0xffC8E6C9)
+val Green66B266 = Color(0xff66B266)
+val Gray333333 = Color(0xff333333)
