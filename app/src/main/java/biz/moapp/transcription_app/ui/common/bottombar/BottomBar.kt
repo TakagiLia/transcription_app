210c5231b18cf69e356d433d103896f58990d375
TakagiRia <r.takagi.test@gmail.com>
2024-07-31 17:35:12 +0900

    BottomBarの追加

---
+++ app/src/main/java/biz/moapp/transcription_app/ui/common/bottombar/BottomBar.kt
@@ -0,0 +1,70 @@
+package biz.moapp.transcription_app.ui.common.bottombar
+
+import android.util.Log
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.navigation.NavHostController
+import biz.moapp.transcription_app.navigation.Nav
+import biz.moapp.transcription_app.ui.theme.BottomBarIconColor
+import biz.moapp.transcription_app.ui.theme.Gray333333
+import biz.moapp.transcription_app.ui.theme.Viridian
+
+@Composable
+fun BottomBar(navController : NavHostController){
+
+    var selectedItemIndex by rememberSaveable {
+        mutableStateOf(0)
+    }
+
+    NavigationBar(containerColor = Viridian, contentColor = Gray333333) {
+        NAVIGATION_ITEMS.forEachIndexed { index, item ->
+            NavigationBarItem(
+                colors = BottomBarIconColor,
+                selected = selectedItemIndex == index,
+                onClick = {
+                    selectedItemIndex = index
+
+                    when(selectedItemIndex){
+                        0 -> navController.navigate(Nav.MainScreen.name)
+                        1 -> Log.d("--BottomBar","center")
+                        2 -> Log.d("--BottomBar","right")
+                    }
+                },
+                label = {
+                    Text(text = item.title, color = Gray333333)
+                },
+                alwaysShowLabel = false,
+                icon = {
+                    BadgedBox(
+                        badge = {
+                            if (item.badgeCount != null) {
+                                Badge {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            } else if (item.hasNews) {
+                                Badge()
+                            }
+                        }
+                    )
+                    {
+                        Icon(
+                            imageVector = if (index == selectedItemIndex) {
+                                item.selectedIcon
+                            } else item.unselectedIcon,
+                            contentDescription = item.title
+                        )
+                    }
+                },
+            )
+        }
+    }
+}
---
+++ app/src/main/java/biz/moapp/transcription_app/ui/common/bottombar/BottomBarUtil.kt
@@ -0,0 +1,42 @@
+package biz.moapp.transcription_app.ui.common.bottombar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.ui.graphics.vector.ImageVector
+
+/**BottomBarのアイテムのデータクラス**/
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
+
+/**BottomBarのアイテムのリスト**/
+val NAVIGATION_ITEMS = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home,
+        hasNews = false,
+    ),
+    BottomNavigationItem(
+        title = "Chat",
+        selectedIcon = Icons.Filled.Email,
+        unselectedIcon = Icons.Outlined.Email,
+        hasNews = false,
+        badgeCount = 45
+    ),
+    BottomNavigationItem(
+        title = "Settings",
+        selectedIcon = Icons.Filled.Settings,
+        unselectedIcon = Icons.Outlined.Settings,
+        hasNews = true,
+    ),
+)
