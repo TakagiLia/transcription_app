c143452ec68e5fc196210232d833375cf6e76b39
TakagiRia <r.takagi.test@gmail.com>
2024-07-31 17:03:44 +0900

    TopBarのUI追加

---
+++ app/src/main/java/biz/moapp/transcription_app/ui/common/TopBar.kt
@@ -0,0 +1,52 @@
+package biz.moapp.transcription_app.ui.common
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.navigation.NavHostController
+import biz.moapp.transcription_app.navigation.Nav
+import biz.moapp.transcription_app.ui.theme.Gray333333
+import biz.moapp.transcription_app.ui.theme.Viridian
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(navigateBack : NavHostController){
+
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "Transcription App",
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Viridian,
+            navigationIconContentColor = Gray333333,
+            titleContentColor = Gray333333,
+            actionIconContentColor = Gray333333
+        ),
+        navigationIcon = {
+            IconButton(onClick = {navigateBack.navigate(Nav.MainScreen.name)}) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Localized description"
+                )
+            }
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Filled.AccountCircle,
+                contentDescription = "Localized description"
+            )
+        },
+    )
+
+}
