efd1eae87c2918712a879a5ceb8371aaa5930c3f
TakagiRia <r.takagi.test@gmail.com>
2024-08-02 18:00:42 +0900

    ナビゲーションバーが他の操作をした際に連動するようにする

--- app/src/main/java/biz/moapp/transcription_app/ui/common/bottombar/BottomBarUtil.kt
+++ app/src/main/java/biz/moapp/transcription_app/ui/common/bottombar/BottomBarUtil.kt
@@ -5,9 +5,11 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Summarize
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.ui.graphics.vector.ImageVector
+import biz.moapp.transcription_app.navigation.Nav
 
 /**BottomBarのアイテムのデータクラス**/
 data class BottomNavigationItem(
+    val screenName: String,
     val selectedIcon: ImageVector,
     val unselectedIcon: ImageVector,
     val hasNews: Boolean,
@@ -16,11 +18,13 @@ data class BottomNavigationItem(
 /**BottomBarのアイテムのリスト**/
 val NAVIGATION_ITEMS = listOf(
     BottomNavigationItem(
+        screenName = Nav.MainScreen.name,
         selectedIcon = Icons.Filled.Home,
         unselectedIcon = Icons.Outlined.Home,
         hasNews = false,
     ),
     BottomNavigationItem(
+        screenName = Nav.SummaryScreen.name,
         selectedIcon = Icons.Filled.Summarize,
         unselectedIcon = Icons.Filled.Summarize,
         hasNews = false,
