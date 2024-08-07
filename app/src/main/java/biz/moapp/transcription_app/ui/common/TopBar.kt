cdcd48754548e3926691723cbe2cc12b6bff8cc7
TakagiRia <r.takagi.test@gmail.com>
2024-08-07 16:32:40 +0900

    AI議事録上のAppBar戻るボタン不要のため削除

--- app/src/main/java/biz/moapp/transcription_app/ui/common/TopBar.kt
+++ app/src/main/java/biz/moapp/transcription_app/ui/common/TopBar.kt
@@ -1,11 +1,7 @@
 package biz.moapp.transcription_app.ui.common
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,7 +10,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import biz.moapp.transcription_app.R
-import biz.moapp.transcription_app.navigation.Nav
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,16 +25,16 @@ fun TopBar(navigateBack : NavHostController){
                 fontWeight = FontWeight.Bold
             )
         },
-        navigationIcon = {
-            if(selectedRoute != Nav.MainScreen.name){
-                IconButton(onClick = {navigateBack.navigate(Nav.MainScreen.name)}) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = "Localized description"
-                    )
-                }
-            }
-        },
+//        navigationIcon = {
+//            if(selectedRoute != Nav.MainScreen.name){
+//                IconButton(onClick = {navigateBack.navigate(Nav.MainScreen.name)}) {
+//                    Icon(
+//                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+//                        contentDescription = "Localized description"
+//                    )
+//                }
+//            }
+//        },
 //        actions = {
 //            Icon(
 //                imageVector = Icons.Filled.AccountCircle,
