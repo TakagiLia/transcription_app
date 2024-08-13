afe0d6ea624be0dedeaec489636b1d725332a4da
Lea <92248493+TakagiLia@users.noreply.github.com>
2024-08-13 15:55:46 +0900

    1.7(8)のリリース (#25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "biz.moapp.transcription_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
