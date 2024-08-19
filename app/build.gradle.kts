8e58c90b8459ab68f59705822b6d5afb3a576035
TakagiRia <r.takagi.test@gmail.com>
2024-08-19 22:03:31 +0900

    1.8(9)のリリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "biz.moapp.transcription_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
