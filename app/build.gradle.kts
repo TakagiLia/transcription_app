2c58166c66a797f96068ae73212ceb7bfa1d3794
TakagiRia <r.takagi.test@gmail.com>
2024-08-27 12:47:50 +0900

    2.6(17)のリリース

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "biz.moapp.transcription_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 16
-        versionName = "2.5"
+        versionCode = 17
+        versionName = "2.6"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
